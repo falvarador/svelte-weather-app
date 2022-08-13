210f74a7392c155fb9e83c1db0378bdef7975650
Freddy Alvarado R <alvaguez1990@gmail.com>
2022-08-12 18:27:37 -0600

    Purge css to load fast

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 import path from 'node:path'
-// import preprocess from 'svelte-preprocess'
 
 export default defineConfig({
   base: '/svelte-weather-app/',
   plugins: [svelte()],
-  // plugins: [svelte({
-  //   preprocess: preprocess({
-  //     postcss: {
-  //       plugins: [
-  //         require('@fullhuman/postcss-purgecss')({
-  //           content: ['./**/*.html', './**/*.svelte']
-  //         })
-  //       ]
-  //     }
-  //   })
-  // })],
   resolve: {
     alias: {
       $: path.resolve('./src')
